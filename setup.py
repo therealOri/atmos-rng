72414e621a66a04d3b2d9511e6edad91281b7de6
Ori <45724082+therealOri@users.noreply.github.com>
2023-06-26 15:38:33 -0600

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,19 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="Atmos",
+    version="1.0.0",
+    author="therealOri",
+    license="GPL-3.0",
+    install_requires=[
+        "requests",
+    ],
+    author_email="therealOri@duck.com",
+    description="A simple random generator based off of atmospheric noise instead of math.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/therealOri/Atmos",
+)
